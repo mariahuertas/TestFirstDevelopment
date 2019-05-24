75ca9c2774715277b692548575d0069587f80932
maria <mariahuertas296@gmail.com>
2019-05-24 11:26:50 +0200

    TFD: fix

--- test/EmptyGameBuilder.java
+++ test/EmptyGameBuilder.java
@@ -20,12 +20,8 @@ public class EmptyGameBuilder {
 		}
 		return this;
 	}
-<<<<<<< HEAD
-	public Game build() {
-=======
 	
-	public Game game() {
->>>>>>> a5e94153dd729aacb29618651f6499bebb850390
+	public Game build() {
 		return this.game;
 	}
 }
